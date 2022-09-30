ea89dacc3df7f716b27c4ae6b1b20eccb42ad8ec
Timothy Sutton <tim@synthist.net>
2022-09-30 12:19:41 -0400

    Build for both amd64 and arm64 on the 3 main platforms

--- build.sh
+++ build.sh
@@ -3,30 +3,32 @@
 set -eux
 
 NAME=speedwagon
+PLATFORMS=( linux windows )
+ARCHS=( amd64 arm64)
+BUILD_DIR=build
 
-PLATFORMS=(
-    darwin
-    linux
-    windows
-)
+version=$(go run main.go version)
+
+rm -rf "${BUILD_DIR}" && mkdir "${BUILD_DIR}"
 
 for platform in "${PLATFORMS[@]}"; do
-    exe_name="${NAME}_${platform}"
+    for goarch in "${ARCHS[@]}"; do
+        exe_name="${NAME}-${version}-${platform}-${goarch}"
 
-    # Windows should have an .exe at the end
-    if [[ "${platform}" = "windows" ]]; then
-        exe_name="${exe_name}.exe"
-    fi
+        # Windows should have an .exe at the end
+        if [[ "${platform}" = "windows" ]]; then
+            exe_name="${exe_name}.exe"
+        fi
 
-    # macOS should be a universal binary
-    if [[ "${platform}" = "darwin" ]]; then
-        for goarch in arm64 amd64; do
-            GOARCH="${goarch}" go build -o "${NAME}_${goarch}"
-        done
-        lipo -create -output "${exe_name}" "${NAME}_arm64" "${NAME}_amd64"
-        rm -f "${NAME}_arm64" "${NAME}_amd64"
-    else
-        GOOS="${platform}" go build -o "${exe_name}"
-    fi
+        GOOS="${platform}" go build -o "${BUILD_DIR}/${exe_name}"
+    done
 done
 
+# macOS should be a universal binary, so we just handle it separately
+platform=darwin
+exe_name="${NAME}-${version}-${platform}-universal"
+for goarch in arm64 amd64; do
+    GOARCH="${goarch}" go build -o "${BUILD_DIR}/${NAME}_${goarch}"
+done
+lipo -create -output "${BUILD_DIR}/${exe_name}" "${BUILD_DIR}/${NAME}_arm64" "${BUILD_DIR}/${NAME}_amd64"
+rm -f "${BUILD_DIR}/${NAME}_arm64" "${BUILD_DIR}/${NAME}_amd64"
