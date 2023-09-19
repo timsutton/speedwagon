166f7d765080ecf9958722e79c126ac2602a3830
Timothy Sutton <tim@synthist.net>
2023-09-19 05:07:24 -0400

    WIP smoke test script

---
+++ script/smoke_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -eux
+
+function cleanup() {
+    rm -f tvOS*
+}
+
+trap cleanup EXIT
+
+function build() {
+    go build
+}
+
+function test() {
+    ./speedwagon list
+
+    (
+        version=$(./speedwagon version)
+        grep -q $version util/version.go
+    )
+
+    # # smallest 'package' type
+    ./speedwagon download 'tvOS 12.4 Simulator'
+
+    # # smallest 'diskImage' type
+    # ./speedwagon download 'tvOS 16 Simulator Runtime'
+}
+
+build
+test
