0b230133d124488152385f247875d00c57ead2b1
Timothy Sutton <tim@synthist.net>
2023-09-24 14:12:24 -0400

    Remove some boilerplate

--- cmd/download.go
+++ cmd/download.go
@@ -103,14 +103,4 @@ func findMatchingRuntime(runtimeName string, data util.DVTDownloadablePlist) (st
 
 func init() {
 	rootCmd.AddCommand(downloadCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// downloadCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// downloadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
